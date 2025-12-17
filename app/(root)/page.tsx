3d92a5d0bc32ff4749b5e17d03da7a9a94ae7045
anhng1106 <ntvanh1106@gmail.com>
2025-12-17 22:08:52 +0200

    created product card for product info

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,17 @@
+import sampleData from "@/db/sample-data";
+import ProductList from "@/components/shared/product/product-list";
+
 const Homepage = () => {
-  return <>Prostore</>;
+  console.log(sampleData);
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrivals"
+        limit={4}
+      />
+    </>
+  );
 };
 
 export default Homepage;
