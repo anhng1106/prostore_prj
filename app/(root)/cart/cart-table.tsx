535fca30527acb96cb5689f49b8f63cbfe8cebf6
anhng1106 <ntvanh1106@gmail.com>
2026-02-25 00:09:50 +0200

    created page for cart items

---
+++ app/(root)/cart/cart-table.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useRouter } from "next/router";
+import { toast } from "sonner";
+import { useTransition } from "react";
+import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
+import { ArrowRight, Loader, Minus, Plus } from "lucide-react";
+import Link from "next/link";
+import Image from "next/image";
+import { Cart } from "@/types";
+
+const CartTable = ({ cart }: { cart?: Cart }) => {
+  //   const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+
+  return (
+    <>
+      <h1 className="py-4 h2-bold">Shopping Cart</h1>
+      {!cart || cart.items.length === 0 ? (
+        <div>
+          Cart is empty. <Link href="/">Go shopping now!</Link>
+        </div>
+      ) : (
+        <div className="grid md:grid-cols-4 md:gap-5">
+          <div className="overflow-x-auto md:col-span-3">Table</div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default CartTable;
